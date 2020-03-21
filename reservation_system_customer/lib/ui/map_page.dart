c58b7e13079642eb824b3cf5b45bb4ad252389d4
Johannes Gundlach <j.gundlach@me.com>
2020-03-21 11:17:49 +0100

    MapPage now uses ReservationsBloc to display the Markers

--- reservation_system_customer/lib/ui/map_page.dart
+++ reservation_system_customer/lib/ui/map_page.dart
@@ -2,23 +2,53 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:reservation_system_customer/repository/repository.dart';
+import 'package:reservation_system_customer/bloc/bloc.dart';
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MapView();
+    return BlocBuilder<ReservationsBloc, ReservationsState>(
+        builder: (context, state) {
+      if (state is ReservationsInitial) {
+        return Container();
+      } else if (state is ReservationsLoading) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if (state is ReservationsLoaded) {
+        Map<MarkerId, Marker> markers = {};
+
+        state.reservations.forEach((reservation) {
+          markers[MarkerId(reservation.id)] = Marker(
+            markerId: MarkerId(reservation.id),
+            position: reservation.location,
+            infoWindow: InfoWindow(
+                title: reservation.locationName,
+                snippet: "A Short description"),
+            onTap: () {},
+          );
+        });
+
+        return MapView(
+          markers: markers,
+        );
+      }
+      return Container();
+    });
   }
 }
 
 class MapView extends StatefulWidget {
+  final Map<MarkerId, Marker> markers;
+
+  const MapView({Key key, this.markers}) : super(key: key);
+
   @override
   State<MapView> createState() => MapViewState();
 }
 
 class MapViewState extends State<MapView> {
   Position position;
-  Map<MarkerId, Marker> markers;
   Completer<GoogleMapController> _controller = Completer();
 
   @override
@@ -30,14 +60,13 @@ class MapViewState extends State<MapView> {
 
   @override
   void initState() {
-    _getMarkers();
     _getCurrentLocation();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (markers == null || position == null) {
+    if (position == null) {
       return Center(child: CircularProgressIndicator());
     } else {
       return GoogleMap(
@@ -49,29 +78,9 @@ class MapViewState extends State<MapView> {
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-        markers: Set<Marker>.of(markers.values),
+        markers: Set<Marker>.of(widget.markers.values),
       );
     }
-  }
-
-  _getMarkers() async {
-    final List<Reservation> reservations =
-        await ReservationsRepository().getReservations();
-    Map<MarkerId, Marker> tmpMap = {};
-
-    reservations.forEach((reservation) {
-      tmpMap[MarkerId(reservation.id)] = Marker(
-        markerId: MarkerId(reservation.id),
-        position: reservation.location,
-        infoWindow: InfoWindow(
-            title: reservation.locationName, snippet: "A Short description"),
-        onTap: () {},
-      );
-    });
-
-    setState(() {
-      markers = tmpMap;
-    });
   }
 
   void _getCurrentLocation() async {
