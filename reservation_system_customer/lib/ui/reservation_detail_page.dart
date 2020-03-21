9cd11bb79bc598b957a0b48d30df49f45366685c
LukkyLukke <lukas.dippon@gmail.com>
2020-03-21 15:13:19 +0100

    Added basic functionality (QR Code)

--- reservation_system_customer/lib/ui/reservation_detail_page.dart
+++ reservation_system_customer/lib/ui/reservation_detail_page.dart
@@ -1,27 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:reservation_system_customer/bloc/bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../repository/data/data.dart';
 
-class ReservationDetailPage extends StatelessWidget {
+class ReservationDetailPage extends StatefulWidget {
   final Reservation reservation;
 
   ReservationDetailPage({this.reservation});
 
+  @override
+  _ReservationDetailPageState createState() => _ReservationDetailPageState();
+}
+
+class _ReservationDetailPageState extends State<ReservationDetailPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool reminder = false;
+  Widget reminderIcon = Icon(Icons.notifications_off);
+//  TODO actually remind the user
+  SnackBar reminderSnackBar = SnackBar(
+    content: Text("Erinnerung 30 Minuten vor deinem Termin"),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text(reservation.location.name),
+        title: Text(widget.reservation.location.name),
+        actions: <Widget>[
+          IconButton(
+            icon: reminderIcon,
+            onPressed: () {
+              _reminderSwitch();
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.delete_outline),
+            onPressed: () {
+              Navigator.pop(context); //TODO delete reservation
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Icon(Icons.arrow_back),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            QrImage(
+              data: widget.reservation.id,
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+            ),
+            Row(
+              children: <Widget>[
+                Text("Erinnern"),
+                Switch(
+                  value: reminder, // TODO!
+                  onChanged: (value) {
+                    setState(() {
+                      reminder = value;
+                    });
+                  },
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  void _reminderSwitch() {
+    setState(() {
+      reminder = !reminder;
+      if (reminder) {
+        reminderIcon = Icon(Icons.notifications_active);
+        _scaffoldKey.currentState.showSnackBar(reminderSnackBar);
+      } else {
+        reminderIcon = Icon(Icons.notifications_off);
+      }
+    });
   }
 }
