e66ef7287534bc800301b0e10d8c39790ad0ac2d
LukkyLukke <lukas.dippon@gmail.com>
2020-03-21 16:39:52 +0100

    cleaned up, moved QR to dialog

--- reservation_system_customer/lib/ui/reservation_detail_page.dart
+++ reservation_system_customer/lib/ui/reservation_detail_page.dart
@@ -2,8 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:reservation_system_customer/bloc/bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../repository/data/data.dart';
+
+Future<void> _ackAlert(BuildContext context, String id) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Container(
+        height: 400,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('Dein Ticket:',
+                style: Theme.of(context).textTheme.headline,),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child:  QrImage(
+                  data: id,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
 
 class ReservationDetailPage extends StatefulWidget {
   final Reservation reservation;
@@ -11,17 +46,24 @@ class ReservationDetailPage extends StatefulWidget {
   ReservationDetailPage({this.reservation});
 
   @override
-  _ReservationDetailPageState createState() => _ReservationDetailPageState();
+  _ReservationDetailPageState createState() => _ReservationDetailPageState(reservation);
 }
 
 class _ReservationDetailPageState extends State<ReservationDetailPage> {
+  final DateFormat dateFormat = DateFormat("dd.MM.yyyy");
+  final DateFormat timeFormat = DateFormat("hh:mm");
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final Reservation reservation;
   bool reminder = false;
   Widget reminderIcon = Icon(Icons.notifications_off);
+
 //  TODO actually remind the user
   SnackBar reminderSnackBar = SnackBar(
     content: Text("Erinnerung 30 Minuten vor deinem Termin"),
+    duration: Duration(seconds: 3),
   );
+
+  _ReservationDetailPageState(this.reservation);
 
   @override
   Widget build(BuildContext context) {
@@ -45,28 +87,36 @@ class _ReservationDetailPageState extends State<ReservationDetailPage> {
         ],
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            QrImage(
-              data: widget.reservation.id,
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-            ),
-            Row(
-              children: <Widget>[
-                Text("Erinnern"),
-                Switch(
-                  value: reminder, // TODO!
-                  onChanged: (value) {
-                    setState(() {
-                      reminder = value;
-                    });
-                  },
-                )
-              ],
-            )
-          ],
-        ),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: <Widget>[
+              Text("Du hast einen Shopping-Slot bei"),
+              Text("${reservation.location.name}",
+                style: Theme.of(context).textTheme.headline,),
+              Text("am"),
+              Text("${dateFormat.format(reservation.timeSlot.startTime)}",
+                style: Theme.of(context).textTheme.headline,),
+              Text("um"),
+              Text("${timeFormat.format(reservation.timeSlot.startTime)}",
+                style: Theme.of(context).textTheme.headline,),
+
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: RaisedButton(
+                  child: Icon(Icons.navigation),
+                  onPressed: _navigate,
+                ),
+              )
+            ],
+          ),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.confirmation_number),
+        onPressed: () {
+          _ackAlert(context, reservation.id);
+        },
       ),
     );
   }
@@ -81,5 +131,9 @@ class _ReservationDetailPageState extends State<ReservationDetailPage> {
         reminderIcon = Icon(Icons.notifications_off);
       }
     });
+  }
+
+  void _navigate() {
+//    TODO navigate
   }
 }
