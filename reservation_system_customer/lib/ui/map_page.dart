10a663a654431d00d9ed6766408f0b139e4c2e64
julia <j.ringler@web.de>
2020-03-21 15:17:44 +0100

    Zoom to User Position as soon as it is available

--- reservation_system_customer/lib/ui/map_page.dart
+++ reservation_system_customer/lib/ui/map_page.dart
@@ -4,12 +4,43 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:reservation_system_customer/bloc/bloc.dart';
 
-class MapPage extends StatefulWidget {
+class MapPage extends StatelessWidget {
   @override
-  _MapPageState createState() => _MapPageState();
+  Widget build(BuildContext context) {
+    return BlocBuilder<MapBloc, MapState>(builder: (context, state) {
+      Map<MarkerId, Marker> markers = {};
+      if (state is MapLocationsLoaded) {
+        state.locations.forEach((reservation) {
+          markers[MarkerId(reservation.id)] = Marker(
+            markerId: MarkerId(reservation.id),
+            position: reservation.position,
+            infoWindow: InfoWindow(
+                title: reservation.name, snippet: "A Short description"),
+            onTap: () {},
+          );
+        });
+      }
+      return MapView(
+        markers: markers,
+      );
+    });
+  }
 }
 
-class _MapPageState extends State<MapPage> {
+class MapView extends StatefulWidget {
+  final Map<MarkerId, Marker> markers;
+
+  const MapView({
+    Key key,
+    this.markers,
+  }) : super(key: key);
+
+  @override
+  State<MapView> createState() => MapViewState();
+}
+
+class MapViewState extends State<MapView> {
+  Completer<GoogleMapController> _controller = Completer();
   final positionTimeout = Duration(seconds: 3);
   LatLng userPosition = LatLng(48.160490, 11.555184); // default position
 
@@ -28,24 +59,48 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MapBloc, MapState>(builder: (context, state) {
-      Map<MarkerId, Marker> markers = {};
-      if (state is MapLocationsLoaded) {
-        state.locations.forEach((reservation) {
-          markers[MarkerId(reservation.id)] = Marker(
-            markerId: MarkerId(reservation.id),
-            position: reservation.position,
-            infoWindow: InfoWindow(
-                title: reservation.name, snippet: "A Short description"),
-            onTap: () {},
-          );
-        });
-      }
-      return MapView(
-        initialPosition: userPosition,
-        markers: markers,
-      );
-    });
+    return Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            userPosition.latitude,
+            userPosition.longitude,
+          ),
+          zoom: 14,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: Set<Marker>.of(widget.markers.values),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToLocation,
+        label: Text("Go to location"),
+      ),
+    );
+  }
+
+  Future<void> _goToLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(
+              userPosition.latitude,
+              userPosition.longitude,
+            ),
+            zoom: 15),
+      ),
+    );
+  }
+
+  void _moveCameraToNewPosition(LatLng position, {double zoom = 14.0}) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(target: position, zoom: zoom)));
   }
 
   _fetchLocations(context) async {
@@ -56,9 +111,8 @@ class _MapPageState extends State<MapPage> {
 
     LatLng location = await _getUserPosition();
     if (location != null) {
-      setState(() {
-        userPosition = location;
-      });
+      userPosition = location;
+      _moveCameraToNewPosition(userPosition);
     }
     BlocProvider.of<MapBloc>(context).add(MapLoadLocations(location));
   }
@@ -82,76 +136,5 @@ class _MapPageState extends State<MapPage> {
       } catch (_) {}
     }
     return location;
-  }
-}
-
-class MapView extends StatefulWidget {
-  final Map<MarkerId, Marker> markers;
-  final LatLng initialPosition;
-
-  const MapView({
-    Key key,
-    this.initialPosition,
-    this.markers,
-  }) : super(key: key);
-
-  @override
-  State<MapView> createState() => MapViewState();
-}
-
-class MapViewState extends State<MapView> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  @override
-  void setState(fn) {
-    if (mounted) {
-      super.setState(fn);
-    }
-  }
-
-  Future<void> _goToLocation() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-            target: LatLng(
-              widget.initialPosition.latitude,
-              widget.initialPosition.longitude,
-            ),
-            zoom: 15),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: true,
-        myLocationEnabled: true,
-        mapType: MapType.normal,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(
-            widget.initialPosition.latitude,
-            widget.initialPosition.longitude,
-          ),
-          zoom: 14,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        markers: Set<Marker>.of(widget.markers.values),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToLocation,
-        label: Text("Go to location"),
-      ),
-    );
-  }
-
-  void _moveCameraToNewPosition(LatLng position, {double zoom = 14.0}) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(target: position, zoom: zoom)));
   }
 }
