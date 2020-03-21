88694fa57fcf80cbf75595d69973035581b82702
Constantin Braess <braess@fzi.de>
2020-03-21 22:40:40 +0100

    date selector implemented fixes #22

--- reservation_system_customer/lib/ui/map/location_detail_sheet.dart
+++ reservation_system_customer/lib/ui/map/location_detail_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:reservation_system_customer/repository/repository.dart';
 
 class LocationDetailSheet extends StatefulWidget {
@@ -28,8 +29,8 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -38,6 +39,21 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
                     style: Theme.of(context).textTheme.headline,
                   ),
                 ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Text("Optionen auswählen (" +
+                        (new DateFormat("dd'.' MMM yyyy"))
+                            .format(barplotStartTime) +
+                        ")"),
+                  ),
+                )
               ],
             ),
             Expanded(
@@ -50,8 +66,8 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
                         onPressed: () {
                           setState(() {
                             if (barplotStartTime.isAfter(DateTime.now())) {
-                              barplotStartTime = barplotStartTime.subtract(
-                                  widget.location.slot_duration);
+                              barplotStartTime = barplotStartTime
+                                  .subtract(widget.location.slot_duration);
                             } else {
                               barplotStartTime = DateTime.now();
                             }
@@ -60,43 +76,41 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
                         splashColor: Theme.of(context).accentColor,
                         child: Icon(Icons.arrow_back_ios)),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: BarChart(widget.location
-                        .capacity_utilization.daily_utilization[0]
-                        .get_bar_data(
-                        barplotStartTime, //starttime
-                        8, // datacount
-                        BarChartGroupData(
-                          // Config
-                            x: 0,
-                            barRods: [
-                              BarChartRodData(
-                                  y: 0,
-                                  width: 20,
-                                  color: Colors.blue,
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(5.0),
-                                      topRight: Radius.circular(5.0)))
-                            ],
-                            barsSpace: 5))
-                        .copyWith(
-                        alignment: BarChartAlignment.spaceEvenly,
-                        titlesData: FlTitlesData(
-                            show: true,
-                            bottomTitles: SideTitles(
-                                showTitles: true,
-                                textStyle: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 10),
-                                getTitles: (double value) {
-                                  return widget.location.capacity_utilization
-                                      .daily_utilization[0]
-                                      .get_bar_titles(value, barplotStartTime);
-                                }),
-                            leftTitles: SideTitles(showTitles: false)),
-                        borderData: FlBorderData(show: false))),
-                  ),
+                  BarChart(widget
+                      .location.capacity_utilization.daily_utilization[0]
+                      .get_bar_data(
+                      barplotStartTime, //starttime
+                      7, // datacount
+                      BarChartGroupData(
+                        // Config
+                          x: 0,
+                          barRods: [
+                            BarChartRodData(
+                                y: 0,
+                                width: 25,
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(5.0),
+                                    topRight: Radius.circular(5.0)))
+                          ],
+                          barsSpace: 3))
+                      .copyWith(
+                      alignment: BarChartAlignment.spaceEvenly,
+                      titlesData: FlTitlesData(
+                          show: true,
+                          bottomTitles: SideTitles(
+                              showTitles: true,
+                              textStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                              getTitles: (double value) {
+                                return widget.location.capacity_utilization
+                                    .daily_utilization[0]
+                                    .get_bar_titles(
+                                    value, barplotStartTime);
+                              }),
+                          leftTitles: SideTitles(showTitles: false)),
+                      borderData: FlBorderData(show: false))),
                   Center(
                     widthFactor: 0.25,
                     child: FlatButton(
@@ -104,8 +118,8 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
                       child: Icon(Icons.arrow_forward_ios),
                       onPressed: () {
                         setState(() {
-                          barplotStartTime = barplotStartTime.add(widget
-                              .location.slot_duration);
+                          barplotStartTime = barplotStartTime
+                              .add(widget.location.slot_duration);
                         });
                       },
                     ),
@@ -113,8 +127,41 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
                 ],
               ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                FlatButton(
+                  textColor: Theme
+                      .of(context)
+                      .accentColor,
+                  onPressed: () async {
+                    Future<DateTime> selectedDate = showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2030),
+                      builder: (BuildContext context, Widget child) {
+                        return Theme(
+                          data: ThemeData.dark(),
+                          child: child,
+                        );
+                      },
+                    );
+                    DateTime timepoint = await selectedDate;
+                    setState(() {
+                      barplotStartTime = timepoint;
+                    });
+                  },
+                  child: Text("Anderes Datum auswählen",
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .caption),
+                )
+              ],
+            ),
             Center(
-              child: FlatButton(
+              child: RaisedButton(
                 child: Text("Slot reservieren"),
                 onPressed: () {
                   // todo: reservierung implementieren
