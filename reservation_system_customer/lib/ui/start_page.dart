12acb6076f390953de7bea6a8e5b726593e7638a
julia <j.ringler@web.de>
2020-03-22 17:40:29 +0100

    Loading and Transition of Reservations

--- reservation_system_customer/lib/ui/start_page.dart
+++ reservation_system_customer/lib/ui/start_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:reservation_system_customer/app_localizations.dart';
 import 'package:reservation_system_customer/bloc/bloc.dart';
 import 'package:reservation_system_customer/repository/repository.dart';
+import 'package:reservation_system_customer/ui/loading_page.dart';
 import 'package:reservation_system_customer/ui/reservations/reservations_list_page.dart';
 import 'package:reservation_system_customer/ui/map/map_page.dart';
 
@@ -14,8 +15,6 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
-  int _selectedIndex = 0;
-
   @override
   void initState() {
     super.initState();
@@ -25,6 +24,31 @@ class _StartPageState extends State<StartPage> {
 
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<ReservationsBloc, ReservationsState>(
+        builder: (context, state) {
+      if (state is ReservationsLoaded) {
+        return _HomePage();
+      }
+      return LoadingPage();
+    });
+  }
+}
+
+class _HomePage extends StatefulWidget {
+  @override
+  __HomePageState createState() => __HomePageState();
+}
+
+class __HomePageState extends State<_HomePage> {
+  int _selectedIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    var state = BlocProvider.of<ReservationsBloc>(context).state;
+    if (state is ReservationsLoaded && (state.reservations?.isNotEmpty ?? false)) {
+      _selectedIndex = 0;
+    }
+
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
