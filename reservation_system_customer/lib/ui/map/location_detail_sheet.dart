d6d48905e76a0e6b3a4b5cec37fa26e16c5685da
julia <j.ringler@web.de>
2020-03-22 23:08:38 +0100

    Static Load Time Slot Data

--- reservation_system_customer/lib/ui/map/location_detail_sheet.dart
+++ reservation_system_customer/lib/ui/map/location_detail_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:reservation_system_customer/app_localizations.dart';
 import 'package:reservation_system_customer/bloc/bloc.dart';
+import 'package:reservation_system_customer/repository/data/capacity_utilization.dart';
 import 'package:reservation_system_customer/repository/repository.dart';
 import 'package:reservation_system_customer/ui/map/reservation_slot_selection.dart';
 import 'reservation_confirmation_dialog.dart';
@@ -19,8 +20,15 @@ class LocationDetailSheet extends StatefulWidget {
 }
 
 class _LocationDetailSheetState extends State<LocationDetailSheet> {
-  DateTime barplotDate = DateTime.now();
+  DateTime selectedDate = DateTime.now();
   DateTime selectedTime;
+  List<Timeslot_Data> data;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,48 +41,12 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 75,
-                    child: Image(
-                      image: AssetImage('assets/002-shop.png'),
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        widget.location.name,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline
-                            .copyWith(fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.left,
-                      ),
-                      Text(
-                        widget.location.address ?? '',
-                        style: Theme.of(context).textTheme.subtitle,
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            _LocationInformation(
+              name: widget.location.name,
+              address: widget.location.address,
             ),
-            ReservationSlotSelection(
-              data: widget.location.capacity_utilization
-                  .get_utilization_by_date(barplotDate)
-                  .timeslot_data,
+            _ReservationSlotsWithLoading(
+              data: data,
               slotSize: widget.location.slotSize,
               selectedSlotChanged: (slot) {
                 setState(() {
@@ -83,11 +55,12 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
               },
             ),
             _ChangeDateButton(
-                title: (DateFormat.yMMMMd("en_US")).format(barplotDate),
+                title: (DateFormat.yMMMMd("en_US")).format(selectedDate),
                 dateChanged: (date) {
                   setState(() {
-                    barplotDate = date;
+                    selectedDate = date;
                   });
+                  _fetchData();
                 }),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -105,16 +78,17 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
                     onPressed: selectedTime == null
                         ? null
                         : () {
-                            BlocProvider.of<ReservationsBloc>(context)
-                                .add(MakeReservation(
-                              locationId: widget.location.id,
-                              startTime: selectedTime,
-                            ));
+                            BlocProvider.of<ReservationsBloc>(context).add(
+                              MakeReservation(
+                                locationId: widget.location.id,
+                                startTime: selectedTime,
+                              ),
+                            );
                             return showDialog<void>(
                               context: context,
                               builder: (BuildContext context) {
                                 return ReservationConfirmationDialog(
-                                    widget.location.name, selectedTime);
+                                    widget.location?.name, selectedTime);
                               },
                             );
                           },
@@ -126,6 +100,18 @@ class _LocationDetailSheetState extends State<LocationDetailSheet> {
         ),
       ),
     );
+  }
+
+  _fetchData() {
+    Future.delayed(Duration(seconds: 3)).then((_) {
+      if (mounted) {
+        setState(() {
+          data = widget.location.capacity_utilization
+              .get_utilization_by_date(selectedDate)
+              .timeslot_data;
+        });
+      }
+    });
   }
 }
 
@@ -153,7 +139,7 @@ class _ChangeDateButton extends StatelessWidget {
           initialDate: DateTime.now(),
           firstDate: DateTime.now().subtract(Duration(hours: 1)),
           lastDate: DateTime.now().add(Duration(days: 2)),
-          builder: (BuildContext context, Widget child)  => child,
+          builder: (BuildContext context, Widget child) => child,
         );
         DateTime date = await selectedDate;
 
@@ -161,6 +147,90 @@ class _ChangeDateButton extends StatelessWidget {
           dateChanged(date);
         }
       },
+    );
+  }
+}
+
+class _ReservationSlotsWithLoading extends StatelessWidget {
+  final int slotSize;
+  final List<Timeslot_Data> data;
+  final ValueChanged selectedSlotChanged;
+
+  const _ReservationSlotsWithLoading({
+    Key key,
+    @required this.slotSize,
+    @required this.data,
+    @required this.selectedSlotChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (data == null) {
+      return Container(
+        height: 200,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return ReservationSlotSelection(
+        data: data,
+        slotSize: slotSize,
+        selectedSlotChanged: selectedSlotChanged,
+      );
+    }
+  }
+}
+
+class _LocationInformation extends StatelessWidget {
+  final String name;
+  final String address;
+
+  const _LocationInformation({
+    Key key,
+    @required this.name,
+    @required this.address,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 75,
+            child: Image(
+              image: AssetImage('assets/002-shop.png'),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                name ?? '',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline
+                    .copyWith(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                address ?? '',
+                style: Theme.of(context).textTheme.subtitle,
+                textAlign: TextAlign.left,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
