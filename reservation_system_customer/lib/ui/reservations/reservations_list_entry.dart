164e863456ab54f5be911d0fd1c1f767eb1dff68
Constantin Braess <braess@fzi.de>
2020-03-22 22:22:22 +0100

    styling changes fixes #57

--- reservation_system_customer/lib/ui/reservations/reservations_list_entry.dart
+++ reservation_system_customer/lib/ui/reservations/reservations_list_entry.dart
@@ -17,116 +17,75 @@ class _ReservationListEntryState extends State<ReservationListEntry> {
   @override
   Widget build(BuildContext context) {
     final DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm:ss");
-    return Dismissible(
-        key: Key('${widget.item.id}'),
-        background: Container(
-          color: Colors.red,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+    return Container(
+      height: 120,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Icon(Icons.delete, size: 40))
-              ]),
-        ),
-        direction: DismissDirection.endToStart,
-        confirmDismiss: (direction) async {
-          return await showDialog(
-            context: context,
-            builder: (context) =>
-                AlertDialog(
-                  title: Text(
-                    AppLocalizations.of(context).translate("delete_res_1") +
-                        '${widget.item.location?.name ?? ''}' +
-                        AppLocalizations.of(context).translate("delete_res_2"),
+                Container(
+                  width: 50,
+                  child: Image(
+                    image: AssetImage('assets/002-shop.png'),
+                    fit: BoxFit.fitWidth,
                   ),
-                  actions: <Widget>[
-                    FlatButton(
-                      child: Text(
-                          AppLocalizations.of(context).translate("cancel")),
-                      onPressed: () => Navigator.of(context).pop(false),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          widget.item.location?.name ?? '',
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .headline,
+                          softWrap: true,
+                        ),
+                        Text(
+                          widget.item.location?.address ?? "",
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .caption,
+                          maxLines: 2,
+                          softWrap: true,
+                        ),
+                      ],
                     ),
-                    FlatButton(
-                      child: Text(AppLocalizations.of(context).translate("ok")),
-                      onPressed: () => Navigator.of(context).pop(true),
+                  ),
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 60,
+                      child: FlatButton(
+                        child: Image(
+                          image: AssetImage(
+                            'assets/004-bell.png',
+                          ),
+                          fit: BoxFit.fitWidth,
+                        ),
+                        onPressed: () {},
+                      ),
                     ),
+                    Text(AppLocalizations.of(context).translate("notification"))
                   ],
                 ),
-          );
-        },
-        onDismissed: (direction) async {
-          BlocProvider.of<ReservationsBloc>(context).add(CancelReservation(
-            reservationId: widget.item.id,
-            locationId: widget.item.location.id,
-          ));
-        },
-        child: Container(
-          height: 100,
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Container(
-                    width: 50,
-                    child: Image(
-                      image: AssetImage('assets/002-shop.png'),
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            widget.item.location?.name ?? '',
-                            style: Theme
-                                .of(context)
-                                .textTheme
-                                .headline,
-                            softWrap: true,
-                          ),
-                          Text(
-                            widget.item.location?.address ?? "",
-                            style: Theme
-                                .of(context)
-                                .textTheme
-                                .subtitle,
-                            maxLines: 2,
-                            softWrap: true,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 70,
-                    child: FlatButton(
-                      child: Image(
-                        image: AssetImage(
-                          'assets/004-bell.png',
-                        ),
-                        fit: BoxFit.fitWidth,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Container(
-                    width: 70,
-                    child: FlatButton(
-                      child: Image(
-                          image: AssetImage('assets/school-material.png'),
-                          fit: BoxFit.fitWidth),
-                      onPressed: () {},
-                    ),
-                  )
-                ],
-              ),
-              Text(DateFormat.yMMMMd("de_DE").add_jm().format(widget.item.startTime))
-            ],
+              ],
+            ),
           ),
-        ));
+          Text(
+            DateFormat.yMMMMd("de_DE").add_jm().format(widget.item.startTime),
+            style: TextStyle(fontWeight: FontWeight.bold),)
+        ],
+      ),
+    );
   }
 }
