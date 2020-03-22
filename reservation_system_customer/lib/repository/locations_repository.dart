3e8aa1544f1af3b0d02bedebb301cf34d435df16
julia <j.ringler@web.de>
2020-03-22 15:57:46 +0100

    Test Data

--- reservation_system_customer/lib/repository/locations_repository.dart
+++ reservation_system_customer/lib/repository/locations_repository.dart
@@ -10,6 +10,64 @@ import 'data/data.dart';
 class LocationsRepository {
   final String baseUrl;
 
+  static Capacity_utilization _cap() {
+    Capacity_utilization cu1 = Capacity_utilization();
+    cu1.utilization = 0.9;
+    Daily_Utilization du = Daily_Utilization(date: DateTime.now());
+    for (var i = 1; i < 20; i++) {
+      du.timeslot_data.add(Timeslot_Data(
+          startTime: DateTime.now().add(new Duration(minutes: i * 10)),
+          bookings: i,
+          utilization: i / 20));
+    }
+    cu1.daily_utilization.add(du);
+    return cu1;
+  }
+
+  var locations = [
+    Location(
+        id: 2434234,
+        position: LatLng(48.135124, 11.581981),
+        name: 'REWE',
+        fillStatus: FillStatus.green,
+        slot_duration: Duration(minutes: 30),
+        capacity_utilization: _cap(),
+    ),
+    Location(
+        id: 2434234,
+        position: LatLng(48.131184, 11.590613),
+        name: 'EDEKA',
+        fillStatus: FillStatus.red,
+        slot_duration: Duration(minutes: 30),
+      capacity_utilization: _cap(),
+    ),
+      Location(
+        id: 2434236,
+        position: LatLng(48.138574, 11.588811),
+        name: 'ALDI',
+        fillStatus: FillStatus.green,
+        slot_duration: Duration(minutes: 30),
+        capacity_utilization: _cap(),
+    ),
+    Location(
+        id: 2434237,
+        position: LatLng(48.125513, 11.583406),
+        name: 'LIDL',
+        fillStatus: FillStatus.red,
+        slot_duration: Duration(minutes: 30),
+      capacity_utilization: _cap(),
+    ),
+    Location(
+        id: 2434238,
+        position: LatLng(48.128091, 11.592672),
+        name: 'Kaufland',
+        fillStatus: FillStatus.yellow,
+        slot_duration: Duration(minutes: 30),
+      capacity_utilization: _cap(),
+    ),
+  ];
+
+
   LocationsRepository({@required this.baseUrl});
 
   Future<Location> getStore(int id) async {
@@ -56,8 +114,9 @@ class LocationsRepository {
     @required int radius,
     @required LocationType type,
   }) async {
-    final store1 = await getStore(10);
-    final store2 = await getStore(11);
-    return [store1, store2];
+//    final store1 = await getStore(10);
+//    final store2 = await getStore(11);
+//    return [store1, store2];
+  return locations;
   }
 }
--- reservation_system_customer/lib/repository/reservations_repository.dart
+++ reservation_system_customer/lib/repository/reservations_repository.dart
@@ -1,12 +1,34 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:reservation_system_customer/repository/repository.dart';
 import 'data/data.dart';
 import 'package:http/http.dart' as http;
 
 class ReservationsRepository {
   final String baseUrl;
+
+  List<Reservation> reservations = [
+    Reservation(
+      id: 30,
+      location: Location(
+        id: 3,
+        name: 'REWE',
+        position: LatLng(48.135124, 11.581981),
+      ),
+      startTime: DateTime.now().add(Duration(minutes: 3)),
+    ),
+    Reservation(
+      id: 20,
+      location: Location(
+        id: 10,
+        name: 'LIDL',
+        position: LatLng(47.960490, 11.355184),
+      ),
+      startTime: DateTime.now().add(Duration(hours: 5)),
+    ),
+  ];
 
   ReservationsRepository({
     @required this.baseUrl,
@@ -38,19 +60,29 @@ class ReservationsRepository {
     @required int locationId,
     @required DateTime startTime,
   }) async {
-    var queryParameters = {
-      'locationId': '$locationId',
-      'dateTime': startTime.toIso8601String(),
-      'deviceId': deviceId,
-    };
-    final uri = Uri.https(baseUrl, '/api/Reservation/Reserve', queryParameters);
-    final response = await http.post(uri);
-
-    if (response.statusCode == 200) {
-      print('createReservation: success');
-    } else {
-      print('createReservation: error ${response.statusCode}');
-    }
+    await Future.delayed(Duration(seconds: 1));
+    reservations.add(
+      Reservation(
+        location:
+            Location(name: 'EDEKA', position: LatLng(48.131184, 11.590613)),
+        id: 10,
+        startTime: DateTime.now().add(Duration(days: 1)),
+      ),
+    );
+//    var queryParameters = {
+//      'locationId': '$locationId',
+//      'dateTime': startTime.toIso8601String(),
+//      'deviceId': deviceId,
+//    };
+//    final uri = Uri.https(baseUrl, '/api/Reservation/Reserve', queryParameters);
+//    final response = await http.post(uri);
+//
+//
+//    if (response.statusCode == 200) {
+//      print('createReservation: success');
+//    } else {
+//      print('createReservation: error ${response.statusCode}');
+//    }
   }
 
   Future<List<Reservation>> getReservations({
@@ -62,12 +94,13 @@ class ReservationsRepository {
     final uri = Uri.https(
         baseUrl, '/api/Reservation/ReservationsByDevice', queryParameters);
 
-    final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      print('getReservations: success');
-      return Reservations.fromJson(json.decode(response.body)).reservations;
-    }
-    print('getReservations: error ${response.statusCode}');
-    return [];
+//    final response = await http.get(uri);
+//    if (response.statusCode == 200) {
+//      print('getReservations: success');
+//      return Reservations.fromJson(json.decode(response.body)).reservations;
+//    }
+//    print('getReservations: error ${response.statusCode}');
+//    return [];
+    return reservations;
   }
 }
