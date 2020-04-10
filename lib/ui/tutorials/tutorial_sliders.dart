0a9b696d2d6edb7ba6fc60f0e6b6afb55a57626f
Johannes Gundlach <j.gundlach@me.com>
2020-04-10 15:10:53 +0200

    Add basic gps permission checker ui

--- lib/ui/tutorials/tutorial_sliders.dart
+++ lib/ui/tutorials/tutorial_sliders.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:intro_slider/intro_slider.dart';
 import 'package:intro_slider/slide_object.dart';
 
@@ -57,25 +59,10 @@ class _TutorialSlidersState extends State<TutorialSliders> {
         title: "Location Permission",
         description:
             "You are almost ready to use the app. Please give us access to your location so we can search for stores around you.",
-        centerWidget: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Icon(
-              Icons.my_location,
-              size: 200,
-              color: Colors.white,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            FlatButton(
-              color: Colors.blueAccent,
-              child: Text(
-                "Grant location access",
-              ),
-              onPressed: () {},
-            )
-          ],
+        centerWidget: _LocationPermissionWidget(
+          onAccessGranted: () {
+            print("on access granted");
+          },
         ),
         backgroundColor: Color(0xffFFA500),
       ),
@@ -84,6 +71,137 @@ class _TutorialSlidersState extends State<TutorialSliders> {
     return IntroSlider(
       slides: slides,
       onDonePress: this.onDonePress,
+    );
+  }
+}
+
+class _LocationPermissionWidget extends StatefulWidget {
+  final Function onAccessGranted;
+
+  const _LocationPermissionWidget({Key key, this.onAccessGranted})
+      : super(key: key);
+
+  @override
+  __LocationPermissionWidgetState createState() =>
+      __LocationPermissionWidgetState();
+}
+
+class __LocationPermissionWidgetState extends State<_LocationPermissionWidget> {
+  bool loading = false;
+  bool accessGranted = false;
+  bool enableGrantAccessButton = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermission();
+  }
+
+  _checkPermission() {
+    Geolocator().checkGeolocationPermissionStatus().then((geolocationStatus) {
+      bool access = false;
+      bool enableButton = false;
+
+      print(geolocationStatus);
+
+      switch (geolocationStatus) {
+        case GeolocationStatus.disabled:
+          access = false;
+          enableButton = false;
+          break;
+        case GeolocationStatus.denied:
+          access = false;
+          enableButton = true;
+          break;
+        case GeolocationStatus.granted:
+          access = true;
+          enableButton = false;
+          break;
+        case GeolocationStatus.restricted:
+          access = true;
+          enableButton = false;
+          break;
+        case GeolocationStatus.unknown:
+          access = false;
+          enableButton = true;
+          break;
+      }
+
+      setState(() {
+        accessGranted = access;
+        loading = false;
+        enableGrantAccessButton = enableButton;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      //used so the height stays the same after the transition so the decryption text does not move
+      height: 300,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(
+            Icons.my_location,
+            size: 200,
+            color: Colors.white,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 200),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return FadeTransition(
+                child: child,
+                opacity: animation,
+              );
+            },
+            child: loading
+                ? CircularProgressIndicator()
+                : FlatButton(
+                    color: Colors.blueAccent,
+                    child: Text(
+                      accessGranted
+                          ? "Access granted"
+                          : "Grant location access",
+                      style: Theme.of(context)
+                          .textTheme
+                          .title
+                          .copyWith(color: Colors.white),
+                    ),
+                    onPressed: !enableGrantAccessButton
+                        ? null
+                        : () {
+                            setState(() {
+                              loading = true;
+                            });
+
+                            //show location permission prompt
+
+                            Geolocator()
+                                .getCurrentPosition(
+                                    desiredAccuracy: LocationAccuracy.high)
+                                .then((value) {
+                              print("then: getCurrentPosition $value");
+
+                              setState(() {
+                                accessGranted = true;
+                                loading = false;
+                                enableGrantAccessButton = false;
+                              });
+
+                              widget.onAccessGranted();
+                            }).catchError((error, stackTrace) {
+                              _checkPermission();
+                            });
+                          },
+                  ),
+          )
+        ],
+      ),
     );
   }
 }
