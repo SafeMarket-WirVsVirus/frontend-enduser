eb93f2855ca1ed8d4d71398ca4cc7dfcdcb36809
julia <j.ringler@web.de>
2020-04-13 22:15:37 +0200

    [#122] Update Slot Size to 10 Minutes
    * Updated slot size to 10 minutes
    * Updated startTime for getLocationReservations
    * Wrapped ReserveSlot button with Center for a nicer animation
    * Fixed bug with date picker allowing yesterday to be selected

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static final slotSizeInMinutes = 15;
+  static final slotSizeInMinutes = 10;
   static final baseUrl = 'wirvsvirusretail.azurewebsites.net';
   static final minHoursBeforeNowForReservations = 2;
   static final minDurationBeforeNowBeforeExpired = Duration(hours: 1);
