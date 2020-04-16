ecac5b79336857b398b342cd9384fd530341f048
LukkyLukke <lukas.dippon@gmail.com>
2020-04-16 19:25:53 +0200

    create class for reminder dialog

---
+++ lib/ui/reservations/reservation_notification_dialog.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:reservation_system_customer/ui_imports.dart';
+
+class ReservationNotificationDialog extends StatefulWidget {
+  final ReservationsBloc reservationsBloc;
+
+  const ReservationNotificationDialog({Key key, this.reservationsBloc}) : super(key: key);
+
+  @override
+  _ReservationNotificationDialogState createState() => _ReservationNotificationDialogState();
+}
+
+class _ReservationNotificationDialogState extends State<ReservationNotificationDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
